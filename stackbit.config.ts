1ec70d7f1b159b314f4e75115f3213cd31bdf861
Alexei Jovmir <ajog@yandex.ru>
2025-04-24 16:07:25 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,41 @@
-import { defineStackbitConfig } from '@stackbit/types';
+// stackbit.config.ts
+import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  // ...
+  modelExtensions: [
+    // Static URL paths derived from the model's "slug" field
+    { name: "Page", type: "page", urlPath: "/{slug}" },
+    { name: "Post", type: "page", urlPath: "/blog/{slug}" }
+  ]
+  siteMap: ({ documents, models }) => {
+    // 1. Filter all page models which were defined in modelExtensions
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                case 'Blog':
+                    return 'otherBlog';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
+});
