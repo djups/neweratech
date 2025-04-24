05b5c89acff70ef2fdd17e3377ad739bc6af3c07
Alexei Jovmir <ajog@yandex.ru>
2025-04-24 16:16:30 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,41 +1,29 @@
-// stackbit.config.ts
-import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
+import { GitContentSource } from '@stackbit/cms-git';
+import { defineStackbitConfig } from '@stackbit/types';
+import { allModels } from './.stackbit/models';
 
-export default defineStackbitConfig({
-  // ...
-  modelExtensions: [
-    // Static URL paths derived from the model's "slug" field
-    { name: "Page", type: "page", urlPath: "/{slug}" },
-    { name: "Post", type: "page", urlPath: "/blog/{slug}" }
-  ]
-  siteMap: ({ documents, models }) => {
-    // 1. Filter all page models which were defined in modelExtensions
-    const pageModels = models.filter((m) => m.type === "page")
-
-    return documents
-      // 2. Filter all documents which are of a page model
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      // 3. Map each document to a SiteMapEntry
-      .map((document) => {
-        // Map the model name to its corresponding URL
-        const urlModel = (() => {
-            switch (document.modelName) {
-                case 'Page':
-                    return 'otherPage';
-                case 'Blog':
-                    return 'otherBlog';
-                default:
-                    return null;
-            }
-        })();
-
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  }
+const gitContentSource = new GitContentSource({
+    rootPath: __dirname,
+    contentDirs: ['content'],
+    models: Object.values(allModels),
+    assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+    }
 });
+
+export const sbConfig = defineStackbitConfig({
+    stackbitVersion: '~0.7.0',
+    ssgName: 'nextjs',
+    nodeVersion: '18',
+    contentSources: [gitContentSource],
+    presetSource: {
+        type: 'files',
+        presetDirs: ['.stackbit/presets']
+    },
+    styleObjectModelName: 'ThemeStyle'
+});
+
+export default sbConfig;
